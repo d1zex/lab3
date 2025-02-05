ecf8bd85a29d6979516824fb2302390a0ac02ac6
d1zex <ospanaren@mail.ru>
2025-02-05 18:38:13 +0500

    spy_game

---
+++ 2.8.py
@@ -0,0 +1,14 @@
+def spy_game(nums):
+    sequence = [0, 0, 7]
+    seq_index = 0  
+    for num in nums:
+        if num == sequence[seq_index]:
+            seq_index += 1  
+        if seq_index == len(sequence):  
+            return True
+
+    return False
+
+print(spy_game([1, 2, 4, 0, 0, 7, 5]))  # T
+print(spy_game([1, 0, 2, 4, 0, 5, 7]))  # T
+print(spy_game([1, 7, 2, 0, 4, 5, 0]))  # F
