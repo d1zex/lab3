cea6aee73e3688f261f97c1164c42de9e55c1b12
d1zex <ospanaren@mail.ru>
2025-02-05 18:44:14 +0500

    modified

--- 2.9.py
+++ 2.9.py
@@ -4,5 +4,5 @@ def sphere_volume(radius):
     volume = (4/3) * math.pi * (radius ** 3)
     return volume
 
-radius = 8
+radius = int(input("Enter ur radius: "))
 print(f"The volume of the sphere with radius {radius} is: {sphere_volume(radius)}")
